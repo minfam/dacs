8df5a87715958db77387dcd615c8f74ec1847513
Leon James <minhkhuong160899@gmail.com>
2024-07-06 16:18:29 +0700

    feat: refactor layout

--- src/app/views/main/main.component.ts
+++ src/app/views/main/main.component.ts
@@ -1,4 +1,4 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
@@ -11,19 +11,24 @@ import { MatInputModule } from '@angular/material/input';
     imports: [CommonModule, MatCardModule, MatInputModule, MatButtonModule, ReactiveFormsModule],
     templateUrl: './main.component.html',
     styleUrls: ['./main.component.scss'],
+    providers: [DatePipe],
 })
 export class MainComponent {
+    constructor(public datepipe: DatePipe) {}
+
     indicators = [
         {
             name: 'Bedok Interchange Boarding Berth 3 to 10',
             time: '10:00',
             left: 'DESTINATION',
-            color: '#01161a',
+            badgeColor: '#05161b',
+            color: '#15313b',
         },
         {
             name: '57059 - Opp Sembawang Air Base',
             time: '09:39',
             left: 'NOW',
+            badgeColor: '#013c5c',
             color: '#046a7b',
         },
         {
@@ -31,12 +36,18 @@ export class MainComponent {
             time: '09:45',
             left: 'NEXT',
             right: true,
-            color: '#4a5964',
+            badgeColor: '#828282',
+            color: '#071e24',
+            textStyle: 'lighter',
         },
         {
             name: '48059 - Opp Mandai Agrotech Pk',
             time: '09:52',
-            color: '#2a3943',
+            color: '#071e24',
+            textStyle: 'lighter',
         },
     ];
+
+    currentDate = this.datepipe.transform(new Date(), 'MM/dd/yyyy');
+    currentTime = this.datepipe.transform(new Date(), 'HH:mm:ss');
 }
